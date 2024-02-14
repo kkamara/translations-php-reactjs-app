3ee3f6a19f8880ddab868d087b6e3251ecc85a17
kkamara <kamaracomputers@gmail.com>
2024-02-14 14:57:13 +0000

    Flesh out translations backend

--- app/Http/Controllers/Web/TranslationsController.php
+++ app/Http/Controllers/Web/TranslationsController.php
@@ -6,13 +6,13 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use App\Http\Requests\CreateTranslations;
-use Illuminate\Support\Facades\App;
+use App\Http\Requests\GetTranslations;
 
 class TranslationsController extends Controller
 {
     public function __construct() {
         $this->middleware('auth:sanctum')
-            ->only(['get']);
+            ->only(['create', 'get']);
     }
 
     /**
@@ -20,14 +20,11 @@ class TranslationsController extends Controller
      */
     public function create(CreateTranslations $request)
     {
-        if ($request->fails()) {
+        if (!$request->validated()) {
             return response()->json([
                 'message' => 'Valid languages are en, es.',
             ], Response::HTTP_BAD_REQUEST);
         }
-
-        $request->session()->put("lang", $request->input("lang"));
-        App::setLocale($request->input("lang"));
 
         return response()->json([
             'message' => 'Success',
@@ -37,10 +34,9 @@ class TranslationsController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function get(Request $request)
+    public function get(GetTranslations $request)
     {
-        // $lang = $request->session()->get("lang");
-        $locale = App::currentLocale();
+        $locale = $request->input("lang");
         dd($locale);
     }
 }
